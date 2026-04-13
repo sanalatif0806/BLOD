087dd12b424108512ac227a6fc9080e5f32bcdc4
sana <sanabati08@gmail.com>
2026-04-13 11:55:26 +0200

    Merge remote-tracking branch 'origin/master_updated' into master_updated
    # Conflicts:
    #	WebApp/backend/data/fairness-data.csv
    #	WebApp/backend/src/env-example
    #	WebApp/frontend/blod/src/api.js
    #	WebApp/frontend/blod/src/components/Graph.js
    #	WebApp/frontend/blod/src/index.js
    #	WebApp/frontend/blod/src/pages/cloud.js
    #	WebApp/frontend/blod/src/pages/dashboard.js
    #	WebApp/frontend/blod/src/pages/search.js
    #	python_service/recover_last_analysis.py

--- WebApp/frontend/blod/src/api.js
+++ WebApp/frontend/blod/src/api.js
@@ -6,5 +6,5 @@ export const base_url = process.env.REACT_APP_API_URL || 'http://localhost:5005'
 //export const base_url = process.env.NODE_ENV === 'development'
 //  ? 'http://localhost:5005'
 //  : 'http://backend:5005';
-export const kghb_url = 'https://kgheartbeat.di.unisa.it/KGHEARTBEAT-API'
+export const kghb_url = 'https://kgheartbeat.di.unisa.it/kgheartbeat-api'
 export const dahboard_backend_url = process.env.REACT_APP_DASHBOARD_BACKEND || 'http://localhost:5001'
